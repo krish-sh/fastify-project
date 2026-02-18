f996a1006b1b188ebbeafaf2016c6753bc144938
Krish Sharma <krishsharma3355@gmail.com>
2026-02-18 21:55:22 +0530

    making user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    reserPasswordToken: String,
+    reserPasswordExpiry: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
